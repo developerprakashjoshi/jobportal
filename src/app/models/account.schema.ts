5eb54e7d38ee39a16bc429703ad8a4b8518d39c7
prakash <prakash.joshi@theadroit.in>
2023-08-02 07:32:56 +0530

    account-schema

--- src/app/models/account.schema.ts
+++ src/app/models/account.schema.ts
@@ -4,14 +4,14 @@ export interface IAccount extends Document {
   username: string;
   email: string;
   type:string;
-  job:string;
+  // job:string;
 }
 
 const userSchema: Schema = new mongoose.Schema({
   username: { type: String, required: true },
   email: { type: String, required: true },
   type: { type: String, required: true },
-  job: { type: String, required: true}
+  // job: { type: String, required: true}
 });
 
 const Account: Model<IAccount> = mongoose.model<IAccount>('Account', userSchema);
